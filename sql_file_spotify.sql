24a060f7649bb0d580536e8ddc0fd5f8a9519230
Shivani Prajapati <shivani1pra@gmail.com>
2025-01-22 19:12:53 +0530

    2nd_commit
    updated queries

--- sql_file_spotify.sql
+++ sql_file_spotify.sql
@@ -8,12 +8,12 @@ limit 1
 #What is the average number of streams for songs released in each month of 2023?
 select released_month, 
 case when released_month=1 then "January"
-	 when released_month=2 then "February"
-     when released_month=3 then "March"
-     when released_month=4 then "April"
-     when released_month=5 then "May"
-     when released_month=6 then "June"
-     when released_month=7 then "July"
+	when released_month=2 then "February"
+     	when released_month=3 then "March"
+     	when released_month=4 then "April"
+     	when released_month=5 then "May"
+     	when released_month=6 then "June"
+     	when released_month=7 then "July"
      end as Month_name, 
 round(avg(streams), 0) as avg_streams 
 from spotify_dataset.spotify_2023
@@ -22,6 +22,16 @@ group by 1
 order by 1 
 
 #Are songs with higher danceability percentages generally more popular (i.e. have more streams)?
-
+select 
+case 
+    when danceability_per >=0 and danceability_per<=25 then "<=25%"
+    when danceability_per>25 and danceability_per<=50 then "between 25% and 50%"
+    when danceability_per > 50 and danceability_per<=75 then "between 50% and 75%"
+    when danceability_per >75 and danceability_per<=100 then "between 75% and 100%"
+    end as danceability_per,
+    round(avg(streams), 2) as popularity 
+    from spotify_dataset.spotify_2023 
+    group by 1 
+    order by 2 desc
 
 #What percentage of songs in the 'Top Spotify Songs 2023' were actually released in 2023?
